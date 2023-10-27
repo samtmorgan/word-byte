c9b8ce77a4ba0bbfa9b5a96d80a8d96f55d899e3
samtmorgan <sam.morgan@beacon-tech.co.uk>
2023-10-27 18:26:19 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
-"use client";
-import Image from "next/image";
+'use client';
+
+import React from 'react';
 
 export default function Home() {
   const speak = () => {
     const msg = new SpeechSynthesisUtterance();
-    msg.text = "Hello World";
+    msg.text = 'Hello World';
     window.speechSynthesis.speak(msg);
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
-        {/* <button onClick={speak}>Speak</button> */}
         <h1>Hi, I am Word Byte</h1>
+        <button type="button" onClick={speak}>
+          speak
+        </button>
       </div>
     </main>
   );
