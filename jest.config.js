0ef0dcfa3d956f040606b53a42aa17e60caca27c
samtmorgan <sam.morgan@beacon-tech.co.uk>
2025-01-01 10:34:13 +0000

    tests: add coverage thresholds, add coverage flag to 'test' script

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '/__utils__/'],
   testEnvironment: 'jsdom',
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 65,
+      lines: 75,
+      statements: 70,
+    },
+  },
 };
