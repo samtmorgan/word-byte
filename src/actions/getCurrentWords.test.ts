023b531f7a7fa49c0843b58fc37654dda8c317bf
samtmorgan <sam.morgan@beacon-tech.co.uk>
2025-02-21 09:09:16 +0000

    feat(getCurrentWords): integrate buildWordSetData and update null handling

--- src/actions/getCurrentWords.test.ts
+++ src/actions/getCurrentWords.test.ts
@@ -1,15 +1,25 @@
 import { mockUser } from '../testUtils/mockData';
+import { buildWordSetData } from '../utils/buildWordSetData';
 import { getCurrentWords } from './getCurrentWords';
 import { initialiseUser } from './initUser';
 
 jest.mock('./initUser', () => ({
   initialiseUser: jest.fn(),
 }));
+jest.mock('../utils/buildWordSetData', () => ({
+  buildWordSetData: jest.fn(),
+}));
 
 describe('getCurrentWords', () => {
+  beforeEach(() => {
+    (buildWordSetData as jest.Mock).mockReturnValue([
+      { owner: 'platform', results: [], word: 'mockWord', wordId: 'mockWordId' },
+    ]);
+  });
   afterEach(() => {
     jest.resetAllMocks();
   });
+
   it('should return current words from initUser', async () => {
     (initialiseUser as jest.Mock).mockResolvedValue(mockUser);
 
@@ -19,12 +29,24 @@ describe('getCurrentWords', () => {
     expect(initialiseUser).toHaveBeenCalledTimes(1);
   });
 
-  it('should return empty array if initUser returns no currentWords', async () => {
+  it('should return null if initUser returns no currentWords', async () => {
     (initialiseUser as jest.Mock).mockResolvedValue(null);
 
     const result = await getCurrentWords();
 
     expect(result).toEqual(null);
     expect(initialiseUser).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return null if buildWordSetData returns null', async () => {
+    (buildWordSetData as jest.Mock).mockReturnValue(null);
+    (initialiseUser as jest.Mock).mockResolvedValue(mockUser);
+
+    const result = await getCurrentWords();
+
+    expect(result).toEqual(null);
+    expect(initialiseUser).toHaveBeenCalledTimes(1);
+    expect(buildWordSetData).toHaveBeenCalledTimes(1);
+    expect(buildWordSetData).toHaveBeenCalledWith(mockUser.wordSets, mockUser.words, 0);
   });
 });
--- src/actions/getCurrentWords.ts
+++ src/actions/getCurrentWords.ts
@@ -1,14 +1,15 @@
 'use server';
 
+import { buildWordSetData } from '../utils/buildWordSetData';
 import { initialiseUser } from './initUser';
 import { User, Word } from './types';
 
 export async function getCurrentWords(): Promise<Word[] | null> {
   const user: User | null = await initialiseUser();
 
-  const currentWords = user?.wordSets[0]?.wordIds
-    .map(wordId => user.words.find(word => word.wordId === wordId))
-    .filter(word => word !== undefined) as Word[];
+  if (!user) return null;
+
+  const currentWords = buildWordSetData(user?.wordSets, user?.words, 0);
 
   if (!currentWords) return null;
 
