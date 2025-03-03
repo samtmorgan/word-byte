9f3655b1e8a00b1311e2815737d5aa3a60d5f0d6
samtmorgan <sam.morgan@beacon-tech.co.uk>
2025-03-03 17:50:35 +0000

    add word-settings route

--- src/app/word-settings/page.tsx
+++ src/app/word-settings/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { initialiseUser } from '../../actions/initUser';
 import { User } from '../../actions/types';
-import { WordSettingsContent } from '../../components/pageComponents/WordSettingsContent';
+import { WordSettingsContent } from '../../components/pageComponents/WordSettings/WordSettingsContent';
 
 export default async function WordSettings() {
   const user: User | null = await initialiseUser();
