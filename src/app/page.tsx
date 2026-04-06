ed372dfae38f47115a43f2c630d5e75458d37426
samtmorgan <sam.morgan@beacon-tech.co.uk>
2026-04-06 17:55:20 +0100

    refactor: remove commented-out code in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,8 @@ import * as React from 'react';
 import { SignedOut, SignInButton } from '@clerk/nextjs';
 import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
-// import { Button } from '../components';
-// import { getUser } from '../actions/actions';
 
 export default async function Home() {
-  // Get the userId from auth() -- if null, the user is not signed in
   const { userId } = await auth();
 
   if (userId) {
